e8f7151d912201ef49e91b2137640357caf65d78
Chukwu Matthew <codemat.biz@gmail.com>
2024-05-29 10:31:43 +0100

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -136,10 +136,10 @@ const Home = () => {
                 />
 
                 <p className='absolute top-4 left-4 text-lg text-green-600 font-semibold'>₦</p>
-                <div className='absolute -top-2 -right-2'>
-                  <span className='relative flex h-7 w-7 justify-center items-center'>
+                <div className='absolute -top-2 right-0'>
+                  <span className='relative flex h-5 w-5 justify-center items-center'>
                     <span className='animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75'></span>
-                    <span className='relative inline-flex rounded-full h-5 w-5 bg-green-500'></span>
+                    <span className='relative inline-flex rounded-full h-3 w-3 bg-green-500'></span>
                   </span>
                 </div>
 
